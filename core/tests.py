7b367211796288afdefc9bb28cccd888ce7daeba
Daniel Owen <danielowen1@hotmail.co.uk>
2020-05-27 16:36:20 +0100

    Initial test setup

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
+from core.models import RequestedItem
+from core import test_utils
+
+
 # Create your tests here.
+class ModelTests(TestCase):
+    def test_requested_items_default_ordering_priority_descending(self):
+        medium = test_utils.create_requested_item(priority=RequestedItem.MEDIUM)
+        high = test_utils.create_requested_item(priority=RequestedItem.HIGH)
+        low = test_utils.create_requested_item(priority=RequestedItem.LOW)
+        self.assertEqual(list(RequestedItem.objects.all()), [high, medium, low])
