437b351dc01b1a8e7cf8021360500fcaa8cf63ef
Daniel Owen <danielowen1@hotmail.co.uk>
2020-05-28 11:08:15 +0100

    Seperate tests into different modules
    	 Fix RequestedItem ordering test. An account and Requester are
    	 now created automatically when a user is created.

---
+++ core/tests/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from core.tests import utils
+from core.models import RequestedItem, Requester
+
+
+class ModelTests(TestCase):
+    def test_requested_items_default_ordering_priority_descending(self):
+        medium = utils.create_requested_item(priority=RequestedItem.MEDIUM)
+        high = utils.create_requested_item(priority=RequestedItem.HIGH)
+        low = utils.create_requested_item(priority=RequestedItem.LOW)
+        self.assertEqual(list(RequestedItem.objects.all()), [high, medium, low])
--- core/tests/utils.py
+++ core/tests/utils.py
@@ -1,17 +1,18 @@
+import uuid
 from random import randint, choice
 from string import ascii_lowercase
 
 from django.contrib.auth.models import User
 
-from core import models as core_models
+from core.models import Account, Item, Requester, Shopper, RequestedItem
 
 
 def random_int(n=10):
     return randint(0, n)
 
 
-def random_string(n=10):
-    return "".join(choice(ascii_lowercase) for _ in range(n))
+def random_string():
+    return str(uuid.uuid4())
 
 
 def set_if_not_present(dictionary, key, value):
@@ -30,27 +31,27 @@ def get_or_create(model, d):
 def create_account(**kwargs):
     d = kwargs.copy()
     set_if_not_present(d, 'name', random_string())
-    return get_or_create(core_models.Account, d)
+    return get_or_create(Account, d)
 
 
 def create_user(**kwargs):
     d = kwargs.copy()
     set_if_not_present(d, 'username', '%s@%s.com' % (random_string(), random_string()))
     set_if_not_present(d, 'password', random_string())
-    return get_or_create(User, d)
+    user = get_or_create(User, d)
+    return user
 
 
 def create_item(**kwargs):
     d = kwargs.copy()
     set_if_not_present_or_falsish(d, 'name', random_string())
-    return get_or_create(core_models.Item, d)
+    return get_or_create(Item, d)
 
 
 def create_requester(**kwargs):
     d = kwargs.copy()
     set_if_not_present(d, 'user', create_user()),
-    set_if_not_present(d, 'account', create_account())
-    return get_or_create(core_models.Requester, d)
+    return d['user'].requester
 
 
 def create_requested_item(**kwargs):
@@ -58,6 +59,6 @@ def create_requested_item(**kwargs):
     set_if_not_present_or_falsish(d, 'requester', create_requester())
     set_if_not_present_or_falsish(d, 'item', create_item())
     set_if_not_present(d, 'quantity', random_int())
-    set_if_not_present(d, 'priority', core_models.RequestedItem.LOW)
-    return get_or_create(core_models.RequestedItem, d)
+    set_if_not_present(d, 'priority', RequestedItem.LOW)
+    return get_or_create(RequestedItem, d)
 
