323eaa6bff368c8ff3180dfd6a9851a5c08f6b28
Daniel Owen <danielowen1@hotmail.co.uk>
2020-05-28 14:04:55 +0100

    Retrieve SECRET_KEY for environment

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(+fbk3g!33jwaw%v40aom=a+z@@el27h*j0%0q)nvvs#t^16yu'
+SECRET_KEY = os.environ.get('SECRET_KEY', '(+fbk3g!33jwaw%v40aom=a+z@@el27h*j0%0q)nvvs#t^16yu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
